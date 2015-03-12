7f5770de715b774cddff3f751f90303cef3f04bf
Andrew Shaffer <andrew@weo.io>
2015-03-12 16:36:09 -0700

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function(base, opts) {
+  opts = opts || {};
+  return base + '?' + qs({
+    scope: buildScopes(opts.scopes, opts.scopePrefix, opts.scopeDelimiter),
+    client_id: opts.clientId,
+    redirect_uri: opts.redirectUri || window.location.origin,
+    response_type: opts.responseType || 'code'
+  });
+};
+
+function qs(params) {
+  return Object.keys(params).map(function(key) {
+    var val = params[key];
+    return key + '=' + encodeURIComponent(val);
+  }).join('&');
+}
+
+function buildScopes(scopes, prefix, delimiter) {
+  var str = scopes.join(delimiter);
+  return prefix
+    ? prefix + delimiter + str
+    : str;
+}
---
+++ test/index.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var oauthUrl = require('..');
+
+describe('oauth-url', function() {
+  var wnd = {location: {origin: 'http://localhost'}};
+  beforeEach(function() {
+    global.window = wnd;
+  });
+
+  it('should work', function() {
+    var googleUrl = oauthUrl('https://accounts.google.com/o/oauth2/auth', {
+      scopePrefix: 'openid',
+      scopeDelimiter: ' ',
+      scopes: ['profile', 'email'],
+    });
+
+    assert.equal(googleUrl, 'https://accounts.google.com/o/oauth2/auth?scope=openid%20profile%20email&client_id=undefined&redirect_uri=http%3A%2F%2Flocalhost&response_type=code');
+  });
+});
